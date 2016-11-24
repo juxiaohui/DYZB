3c9f5287ca3ba72a0f296f362eca5039c4d9e697
juxiaohui <123456>
2016-11-24 14:39:25 +0800

    install project

---
+++ DYZB/DYZB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYZB
+//
+//  Created by juxiaohui on 16/11/24.
+//  Copyright © 2016年 juxiaohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYZB/DYZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYZB
+//
+//  Created by juxiaohui on 16/11/24.
+//  Copyright © 2016年 juxiaohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
